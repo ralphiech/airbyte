efabeb02615b20e6d50bbf6749e8cfaf53b90e0a
zstipanicev <zoran@LAPTOP-RJBDUFGF.localdomain>
2022-04-02 21:33:49 +0200

    reading source.py to determine entites for which to generate schemas and apply naming convention

--- airbyte-integrations/connectors/source-vtiger/scripts/generate_schemas.py
+++ airbyte-integrations/connectors/source-vtiger/scripts/generate_schemas.py
@@ -10,17 +10,6 @@ def replce_if_dict_elem_exists(in_string, in_dict, in_label ):
         ret = in_string.replace(f"<{in_label}>", in_dict[in_label])
     return ret
 
-def read_json_secrets_file(in_file_name):
-    in_schema_config = f'..{os.sep}secrets{os.sep}{in_file_name}'
-    schema_config = {}
-    cwd = os.getcwd()
-    schema_config_path = os.path.abspath(os.path.join(cwd, in_schema_config))
-    if os.path.exists(schema_config_path):
-        with open(schema_config_path, "r") as config_file:
-            schema_config = json.load(config_file)
-    return schema_config
-
-
 ##########################################################################
 
 def gen_schema(in_json_details):
@@ -64,7 +53,7 @@ def gen_schema(in_json_details):
                   "title": "<label>",
                   "description": "<dblabel>",
                   "default": ""<db_name>
-                }<trailing_comma>
+                }
     """
 
 
@@ -76,7 +65,7 @@ def gen_schema(in_json_details):
     print(entity_name)
     print("========================================")
     all_fields = ""
-    for idx,fld in enumerate(data["result"]["fields"]):
+    for fld in data["result"]["fields"]:
         db_name = ""
         fld_name = fld["name"]
         if entity_name in fields_config:
@@ -90,11 +79,6 @@ def gen_schema(in_json_details):
         field_json = replce_if_dict_elem_exists(field_json, fld, "label")
         field_json = replce_if_dict_elem_exists(field_json, fld, "dblabel")
         field_json = field_json.replace('<db_name>', db_name)
-        if idx + 1 != len(data["result"]["fields"]):
-            field_json = field_json.replace("<trailing_comma>", ",")
-        else:
-            field_json = field_json.replace("<trailing_comma>", "")
-
         all_fields = all_fields + field_json
 
     all_fields = schema_prefix + all_fields + schema_postfix
@@ -128,13 +112,38 @@ def get_entity_details(in_entity):
 
 def write_schema(in_content, in_entity):
     print("write_schema :: " + in_entity)
-    out_file_name = in_entity.replace("vtcm", "vtcm_") + ".json"
+    out_file_name = in_entity #.replace("vtcm", "vtcm_") + ".json"
     cwd = os.getcwd()
     path_schemas = os.path.abspath(os.path.join(cwd, f'..{os.sep}source_vtiger{os.sep}schemas'))
 
     file_path = os.path.join(path_schemas, out_file_name)
     with open(file_path, "w") as write_file:
         write_file.write(in_content)
+
+##########################################################################
+
+def parse_source_script():
+    in_source = f'..{os.sep}source_vtiger{os.sep}source.py'
+
+    schema_file_names = []
+    query_names = []
+    p = re.compile('([A-Z])')
+    with open(in_source, 'r') as f:
+        for line in f:
+            found = re.findall("^class (.+)\(VtigerStream\)", line)
+            if found:
+                schema_file_name = found[0][0] + p.sub(r'_\1', found[0][1:])
+                schema_file_name = schema_file_name.lower()
+                # print(schema_file_name)
+                schema_file_names.append(schema_file_name)
+            found = re.findall("return self\.get_query_url_string\(\'(.+)\', next_page_token\)", line)
+            if found:
+                query_names.append(found[0])
+
+    combined = []
+    for (s, q) in zip(schema_file_names, query_names):
+        combined.append({"query_name":q, "schema_file_name":s})
+    return combined
 
 ##########################################################################
 
@@ -145,8 +154,6 @@ def main():
 
     # sample schema config file
     # {
-    #   "vtiger_types_list": ["Calendar"
-    #                       ],
     #   "fields_config": {
     #     "Calendar": {
     #       "a_field_with_a_really_long_name_and_airbyte_shortens_it_in_db": {
@@ -157,16 +164,19 @@ def main():
     # }
 
     print("=== Main ===")
-    schema_config = read_json_secrets_file("schema.json")
-    if not "vtiger_types_list" in schema_config:
-        print("No vtiger type to process defined in the config file (secrets/schema.json)")
+    # schema_config = read_json_secrets_file("schema.json")
+    entities = parse_source_script()
+    print(len(entities))
+    if len(entities) == 0:
+        print("No classes recognised in the source.py file")
     else:
-        vtiger_types = schema_config["vtiger_types_list"]
-        for entity in vtiger_types:
-            print(f"processing: {entity}")
-            entity_details = get_entity_details(entity)
+        # vtiger_types = schema_config["vtiger_types_list"]
+        for entity in entities:
+            entity_name = entity["query_name"]
+            print(f"processing: {entity_name}")
+            entity_details = get_entity_details(entity_name)
             schema = gen_schema(entity_details)
-            write_schema(schema, entity.lower())
+            write_schema(schema, entity["schema_file_name"])
 
 main()
 
