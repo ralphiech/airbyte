72abeb378aa52894ddc1c96f61b8cf8ad4de23a7
Ralph Schwaninger <ralphie.ch@gmail.com>
2022-08-04 16:14:26 +0200

    introduce central generate.sh file

--- airbyte-integrations/connectors/source-vtiger/generate.sh
+++ airbyte-integrations/connectors/source-vtiger/generate.sh
@@ -1,14 +1,10 @@
-# echo 'delete old schema files'
-# echo '***********************'
-# rm source_vtiger/schemas/*.json -v
+echo 'delete old schema files'
+echo '***********************'
+rm source_vtiger/schemas/*.json -v
 
-# echo 'generate new schema files'
-# echo '*************************'
-# python3 ./scripts/generate_schemas.py
-
-
-
-
+echo 'generate new schema files'
+echo '*************************'
+python3 ./scripts/generate_schemas.py
 
 echo 'remove old ddl (sql) files'
 echo '***********************'
